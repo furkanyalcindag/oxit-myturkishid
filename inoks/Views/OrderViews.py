a49be3103f52f8cac1e6d955f223b232029b9f3d
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2019-12-11 14:45:15 +0300

    iban

--- inoks/Views/OrderViews.py
+++ inoks/Views/OrderViews.py
@@ -631,7 +631,7 @@ def odeme_sonuc(request):
     # to base64
     b = base64.b64encode(b).decode()
 
-    order = Order.objects.get(pk=request.POST.get('merchant_oid'))
+    order = Order.objects.get(pk=int(request.POST.get('merchant_oid')))
 
     if b != request.POST.get("hash"):
         sys.exit()
