70cabbba6e13a7a4811e9cfd92ed5261f220ae46
Furkan Yalçındağ <fyalcindag@oxityazilim.com>
2020-02-17 12:43:43 +0300

    soyagacım yapıldı

--- oxiterp/wsgi.py
+++ oxiterp/wsgi.py
@@ -12,7 +12,7 @@ import os,sys
 from django.core.wsgi import get_wsgi_application
 
 
-sys.path.append('/opt/django-ox/apps/django/django_projects/oxit-inoks')
+#sys.path.append('/opt/django-ox/apps/django/django_projects/oxit-inoks')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oxiterp.settings.prod')
 
 application = get_wsgi_application()
